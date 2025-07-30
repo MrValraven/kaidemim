81aeed51d242d1d621a5598f6a1e68fc3991ab92
mrvalraven <akatiago@gmail.com>
2025-07-30 19:38:47 +0100

    new stuff

--- src/garden.ts
+++ src/garden.ts
@@ -26,6 +26,21 @@ import Photo25 from "./assets/images/photo25.jpeg";
 import Photo26 from "./assets/images/photo26.jpeg";
 import Photo27 from "./assets/images/photo27.jpeg";
 import Photo28 from "./assets/images/photo28.jpeg";
+import Photo29 from "./assets/images/photo29.jpg";
+import Photo30 from "./assets/images/photo30.jpg";
+import Photo31 from "./assets/images/photo31.jpg";
+import Photo32 from "./assets/images/photo32.jpg";
+import Photo33 from "./assets/images/photo33.jpg";
+import Photo34 from "./assets/images/photo34.jpg";
+import Photo35 from "./assets/images/photo35.jpg";
+import Photo36 from "./assets/images/photo36.jpg";
+import Photo37 from "./assets/images/photo37.jpg";
+import Photo38 from "./assets/images/photo38.jpg";
+import Photo39 from "./assets/images/photo39.jpg";
+import Photo40 from "./assets/images/photo40.jpg";
+import Photo41 from "./assets/images/photo41.jpg";
+import Photo42 from "./assets/images/photo42.jpg";
+import Photo43 from "./assets/images/photo43.jpg";
 
 export const images = [
   { photoSource: Photo1, caption: "The cutest queers" },
@@ -62,4 +77,22 @@ export const images = [
   { photoSource: Photo25, caption: "Honestly, love you so much" },
   { photoSource: Photo27, caption: "oop-" },
   { photoSource: Photo28, caption: "Beijinhosss" },
+  { photoSource: Photo32, caption: "Mom, I'm in love with a (carpet) monster" },
+  { photoSource: Photo30, caption: "Just me and my gay bestie 💋" },
+  { photoSource: Photo31, caption: "kissesss" },
+  { photoSource: Photo29, caption: "I love being the fairy of your garden" },
+  { photoSource: Photo33, caption: "This was such a perfect day" },
+  { photoSource: Photo35, caption: "My eyeliner is criminal" },
+  { photoSource: Photo34, caption: "Need this in a photo album" },
+  { photoSource: Photo36, caption: "Confusing people at FMM" },
+  { photoSource: Photo37, caption: "...JUST LIKE MY GIANT DIC-" },
+  { photoSource: Photo38, caption: "Certified Munchers" },
+  { photoSource: Photo39, caption: "Strong like bull!" },
+  { photoSource: Photo42, caption: "LOOK, TWO HOT BISEXUALS!" },
+  {
+    photoSource: Photo41,
+    caption: "Microdosing D&D by being literal chaos goblins",
+  },
+  { photoSource: Photo40, caption: "YOU ARE SO GORGEOUS I WANNA SCREAM" },
+  { photoSource: Photo43, caption: "FREAK(s)" },
 ];
